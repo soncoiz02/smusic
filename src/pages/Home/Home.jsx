1141198dee34a5339791f1730280238034976d62
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-04-06 04:50:49 +0700

    feat: home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <div className="content">
+                <h1 className='title'>Wellcom to <span>SMUSIC</span></h1>
+                <p className="sub-title">Where you can listen to music Free</p>
+                <div className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fugiat consequatur dolore sequi eos laborum quod voluptas voluptates molestiae aspernatur! Minus, sit expedita! Debitis expedita sequi est consequatur nulla error!</div>
+            </div>
+            <div className="top-10">
+                <h2>Top 10 Popular Songs</h2>
+            </div>
+        </div>
     )
 }
 
