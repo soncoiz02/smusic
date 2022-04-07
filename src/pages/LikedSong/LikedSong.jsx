cd3abe4c1d42f7a276d715a6c575c96d6887ed92
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-04-07 16:35:15 +0700

    feat: liked song page

--- src/pages/LikedSong/LikedSong.jsx
+++ src/pages/LikedSong/LikedSong.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const LikedSong = () => {
+    const isLogin = useSelector(state => state.users.isLogin)
+    const userInfor = useSelector(state => state.users.infor)
+    const [likedSongs, setLikedSongs] = useState([])
+
+    useEffect(() => {
+        if (isLogin) {
+            const likedSongs = userInfor.likedSongs
+            setLikedSongs(likedSongs)
+        }
+    }, [])
+
     return (
-        <div>LikedSong</div>
+        <div className='liked-song'>
+            {
+                isLogin ?
+                    <div className='liked-song-content'>
+                        {
+                            likedSongs.length > 0 ?
+                                <>
+                                    <h2>Your liked songs</h2>
+                                </>
+                                : <h2>Looks like you don't like any of the songs</h2>
+                        }
+                    </div>
+                    :
+                    <div className="require-login">
+                        <h2>Please login to see your liked songs</h2>
+                        <svg>
+                            <path className='path' fill='black' strokeWidth={5} ></path>
+                        </svg>
+                    </div>
+            }
+        </div>
     )
 }
 
